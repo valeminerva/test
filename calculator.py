19c7797be6a0ade17faa85cfa252b47a15c55e7d
Miranda LaBounty <mirandakloon@gmail.com>
2016-04-26 21:01:05 -0700

    calculator exercice

---
+++ calculator.py
@@ -0,0 +1,48 @@
+def add(num1,num2):
+	return num1 + num2
+
+def subtract(num1,num2):
+	return num2 - num1
+
+def multiply(num1,num2):
+	return num1 * num2
+
+def divide(num1,num2):
+	return num1/num2
+
+def modulo(num1,num2):
+	return num1%num2
+
+def power (num1,num2):
+	return num1**num2
+
+def square (num1):
+	return power(num1,2)
+
+print square(2)
+
+
+
+print add(add(4,5),subtract(9,6))
+
+print subtract(divide(12/2),60)
+
+print add(1, add(2,3))
+
+
+
+
+
+
+
+# print subtract(108,3672)
+
+# print multiply(17,20)
+
+#print divide(84,14)
+
+#print modulo (8,3)
+
+#print power (2,3)
+
+#print square (4)
